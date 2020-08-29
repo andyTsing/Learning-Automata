ec96f562d65d71268892056dfc3b1205888d93be
Paul Trudel <35616208+paultrudel@users.noreply.github.com>
2020-08-29 17:41:48 -0400

    Update Automaton.java

--- Learning-Automata/src/automata/Automaton.java
+++ Learning-Automata/src/automata/Automaton.java
@@ -3,9 +3,17 @@ package automata;
 import java.util.Random;
 
 import main.Environment.Response;
-
+/**
+Abstract class to encapsulate the shared behaviour of all the types of automata.
+The structure of most learning automata is very similar with most differences coming from how actions
+are chosen and they way in which they respond to responses from the environment.
+**/
 public abstract class Automaton {
 
+	/** 
+	Possible actions the automaton may take in its environment. 
+	Represents the six possible floors the elevator may choose to wait on. 
+	**/
 	public static enum Action {
 		FLOOR_1,
 		FLOOR_2,
@@ -35,6 +43,7 @@ public abstract class Automaton {
 		actionStrength = rand.nextInt(MAX_ACTION_STRENGTH) + 1;
 	}
 	
+	/** Move to the next possible action one confidence in the current action has been reduced to below its minimum value **/
 	protected void nextAction() {
 		int actionIndex = 0;
 		for(int i = 0; i < Action.values().length; i++) {
@@ -48,5 +57,9 @@ public abstract class Automaton {
 	
 	public Action getAction() { return action; }
 	
+	/** 
+	Each automaton adapts uniquely to responses from its environment 
+	and must therefore implement its own method.
+	**/
 	public abstract void adjustAction(Response response);
 }
